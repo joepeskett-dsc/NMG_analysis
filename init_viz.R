fd21f6d898efc89b14ac0c0401104f1246b9ec52
joepeskett-dsc <joseph.peskett@ons.gov.uk>
2018-10-02 22:36:23 +0100

    add split by age code

--- init_viz.R
+++ init_viz.R
@@ -4,31 +4,29 @@ library(reshape2)
 library(dplyr)
 NMG_2017 <- read.csv("data/NMG_2017.csv")
 example <- NMG_2017 %>% 
-  select(X, actdebt1, 
-         actdebt2, actdebt3, 
-         actdebt4, actdebt5, 
-         actdebt6, actdebt7)
-g_data <- melt(example, "X")%>%
-  group_by(variable)%>%
-  count(value)
+  select(c("X", paste0("actdebt", seq(1, 11))), "age_grp")
+g_data <- melt(example, c("X", "age_grp"))%>%
+  group_by(variable, age_grp)%>%
+  rowwise()%>%
+  mutate(count(variable)/count(age_grp))
 g <- ggplot(data = g_data, 
             aes(x = variable, 
                 y = n))
 g+geom_bar(stat = "identity", 
            aes(fill = value))+ 
   theme_minimal() +
-  ylim(-1000,nrow(NMG_2017)) +
+  ylim(-250,2000) +
   theme(
     axis.text = element_blank(),
     axis.title = element_blank(),
     panel.grid = element_blank(),
     plot.margin = unit(rep(-2,4), "cm")     # This remove unnecessary margin around plot
   ) + 
-  coord_polar(start = 0) +
-  geom_text(data=label_data, 
-            aes(x=variable, y=value+10, 
-                label=variable, hjust=hjust), 
-            color="black", fontface="bold",
-            alpha=0.6, size=2.5, 
-            angle= label_data$angle, 
-            inherit.aes = FALSE )
+  coord_polar(start = 0) + facet_grid(cols = vars(age_grp))
+##  geom_text(data=label_data, 
+#            aes(x=variable, y=value+10, 
+#                label=variable, hjust=hjust), 
+#            color="black", fontface="bold",
+#            alpha=0.6, size=2.5, 
+#            angle= label_data$angle, 
+#            inherit.aes = FALSE )
